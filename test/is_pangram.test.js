5b49539e4044216c17a1114724bb455180d21019
Marj E <marj@marjorieetienne.com>
2020-12-10 18:12:04 -0800

    initial commit, added test code for pangram

--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -10,54 +10,84 @@ describe('isPangram()', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
+    const result = isPangram(text);
 
     // Assert
+    expect(result).toBeTruthy();
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
+    const text = "abcdefghijklmnopqrstuvwxyz";
 
     // Act
+    const result = isPangram(text);
 
     // Assert
-
+    expect(result).toBeTruthy();
   });
 
   test("missing character 'x'", () => {
     // Arrange
+    const text = "abcdefghijklmnopqrstuvwyz";
 
     // Act
+    const result = isPangram(text);
 
     // Assert
-
+    expect(result).toBeFalsy();
   });
 
   test('empty sentence', () => {
     // Arrange
+    const text = '';
 
     // Act
+    const result = isPangram(text);
 
     // Assert
+    expect(result).toBeFalsy();
 
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
 
     // Act
+    const result = isPangram(text);
 
     // Assert
+    expect(result).toBeTruthy();
 
   });
 
   test('pangram with numbers', () => {
     // Arrange
-
+    const text = 'abcdefghijklmnopqrstuvwyz49202';
+    
     // Act
+    const result = isPangram(text);
 
     // Assert
+    expect(result).toBeFalsy();
 
   });
 
   // Write your own test case
+  test('pangram with mixed case letters', () => {
+    // Arrange
+    const valid_chars = "abcdefghijklmnopqrstuvwxyz"
+    const text = 'Pack my box with five dozen liquor jugs';
+    
+    // Act
+    const result = isPangram(text);
+
+    // Assert
+    for (let char of text) {
+      expect(valid_chars.includes(char)).toBeTruthy();
+    }
+    expect(result).toBeTruthy();
+
+  });
 });
